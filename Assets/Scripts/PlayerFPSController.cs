af4e57c691d2b140b1c667fd5e067981d97788ae
Andrey Viktorov <andrey.viktorov@gmail.com>
2021-10-02 22:44:37 +0700

    Modified player FPS controller
    Key changes:
    - Added ability to run / walk / jump
    - Removed character controller, used rigid body instead
    - Added naive air drag & max velocity
    - Added naive fov multiplier while running

--- Assets/Scripts/PlayerFPSController.cs
+++ Assets/Scripts/PlayerFPSController.cs
@@ -2,26 +2,39 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+// TODO: jump? run?
+
 public class PlayerFPSController : MonoBehaviour
 {
 	public float turnSpeed = 360.0f;
-	public float movementSpeed = 10.0f;
+	public float walkSpeed = 5.0f;
+	public float runSpeed = 10.0f;
+	public float airSpeed = 5.0f;
+	public float maxSpeed = 10.0f;
+	public float airDrag = 5.0f;
+	public float jumpSpeed = 100.0f;
+	public float fovSmoothness = 5.0f;
+	public float runFovMultiplier = 1.2f;
 	public const float ANGLE_LIMIT = 89.0f;
+
+	private bool onGround = false;
 
 	private Camera cachedCamera;
 	private Transform cachedCameraTransform;
-	private CharacterController cachedCharacterController;
+	private Rigidbody cachedRigidbody;
 
 	private Vector2 angles;
 	private Quaternion initialOrientation;
+	private float initialFov;
 
 	private void Awake()
 	{
 		cachedCamera = GetComponentInChildren<Camera>();
 		cachedCameraTransform = cachedCamera.transform;
-		cachedCharacterController = GetComponent<CharacterController>();
+		cachedRigidbody = GetComponent<Rigidbody>();
 
 		initialOrientation = cachedCameraTransform.localRotation;
+		initialFov = cachedCamera.fieldOfView;
 		angles = Vector2.zero;
 
 		Cursor.lockState = CursorLockMode.Locked;
@@ -30,6 +43,8 @@ public class PlayerFPSController : MonoBehaviour
 
 	private void Update()
 	{
+		bool isSprinting = Input.GetButton("Sprint");
+
 		// aim
 		Vector2 mouseDelta = new Vector2(
 			Input.GetAxis("Mouse X"),
@@ -53,8 +68,46 @@ public class PlayerFPSController : MonoBehaviour
 		if (movement.sqrMagnitude > 1)
 			movement.Normalize();
 
-		movement *= movementSpeed;
+		// jump
+		float verticalVelocity = cachedRigidbody.velocity.y;
 
-		cachedCharacterController.SimpleMove(movement);
+		if (onGround && Input.GetButtonDown("Jump"))
+		{
+			verticalVelocity = jumpSpeed;
+			onGround = false;
+		}
+
+		// apply
+		if (onGround)
+			movement *= isSprinting ? runSpeed : walkSpeed;
+		else
+			movement = Vector3.Lerp(cachedRigidbody.velocity, movement * airSpeed, airDrag * Time.deltaTime);
+
+		movement.y = 0.0f;
+
+		float currentMagnitude = movement.magnitude;
+		movement = movement.normalized * Mathf.Min(maxSpeed, currentMagnitude);
+
+		cachedRigidbody.velocity = new Vector3(movement.x, verticalVelocity, movement.z);
+
+		// camera fov
+		float targetFov = initialFov;
+
+		if (onGround && isSprinting)
+			targetFov = initialFov * runFovMultiplier;
+
+		cachedCamera.fieldOfView = Mathf.Lerp(cachedCamera.fieldOfView, targetFov, fovSmoothness * Time.deltaTime);
+	}
+
+	private void OnCollisionEnter(Collision collision)
+	{
+		foreach (ContactPoint contact in collision.contacts)
+		{
+			if (contact.normal.y > 0.8f)
+			{
+				onGround = true;
+				return;
+			}
+		}
 	}
 }
